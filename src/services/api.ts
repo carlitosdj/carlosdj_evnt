b0579d6ec4412574bdab72d16f918e2876903fec
Carlos Defelicibus Junior <carlitosceo@gmail.com>
2024-01-09 17:56:19 -0300

    Api url change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://institutodefelicibus.com.br/apimodelo",
+  baseURL: "https://institutodefelicibus.com.br/apicarlosdj",
   //baseURL: 'http://localhost:3000',
 });
 
